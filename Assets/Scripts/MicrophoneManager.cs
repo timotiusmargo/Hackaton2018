d776e194e91cfb6d8220151c80c3cdcc4c6b057b
Kishan Trivedi <t-kitri@microsoft.com>
2018-07-24 17:02:58 -0700

    Removed other assets

--- Assets/Scripts/MicrophoneManager.cs
+++ Assets/Scripts/MicrophoneManager.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework.Interfaces;
-using NUnit.Framework.Internal;
+﻿
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
